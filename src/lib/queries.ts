f2bbc42a81d88d0d138273a5c0b9b1ca13cfa63b
uditdesai <udit.desai3@gmail.com>
2022-01-11 22:22:25 -0500

    add info query

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -33,3 +33,7 @@ export const booksQuery = groq`
   slug,
   }
 `;
+
+export const infoQuery = groq`
+*[_type == 'info'][0]{...}
+`
--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next";
+import Sanity from "../lib/config";
+import { infoQuery } from "../lib/queries";
 
-const Info: NextPage = () => {
-  return <div>Info</div>;
+interface InfoProps {
+  info: any;
+}
+
+const Info: NextPage<InfoProps> = ({ info }) => {
+  return <div>{info.title}</div>;
 };
+
+export async function getStaticProps({ params }: any) {
+  const info = await Sanity.fetch(infoQuery);
+
+  return { props: { info } };
+}
 
 export default Info;
