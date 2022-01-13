fd57692d0b748e60678036bc9b0dd5c97699f317
uditdesai <udit.desai3@gmail.com>
2022-01-12 23:13:49 -0500

    fix type problem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next"
 import { Sanity } from "../lib/config"
 import { booksQuery } from "../lib/queries"
 import { Book } from "../components/Book"
+import { Key } from "react"
 
 interface HomeProps {
   books: any
@@ -10,7 +11,7 @@ interface HomeProps {
 const Home: NextPage<HomeProps> = ({ books }) => {
   return (
     <main className="w-100 min-h-screen px-4 md:px-8 pb-8 pt-24 xs:pt-36 md:pt-48 bg-white grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-12 xs:gap-x-24 sm:gap-x-20 lg:gap-x-24 xl:gap-x-28">
-      {books.map((book) => (
+      {books.map((book: any) => (
         <Book key={book._id} book={book} />
       ))}
     </main>
